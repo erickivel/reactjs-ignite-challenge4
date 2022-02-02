09764c9eaae11c7d96b7dc6331421da18adcde50
Eric Kivel <ericvkivel1@gmail.com>
2022-02-02 16:14:16 -0300

    First Attempt

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,22 @@ import {
   useRef,
   useState,
   useCallback,
+  ElementType
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface Props {
+  name: string;
+  icon?: ElementType;
+}
+
+type InputProps = JSX.IntrinsicElements['input'] & Props;
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
